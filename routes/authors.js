c53b183c735b40f474a3c114bc627e70b3540018
SatishTrasula <trasula.satish@gmail.com>
2020-11-09 11:26:03 +0530

    Author show,edit,delete done

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
+const Book = require('../models/book')
 //Search all Authors
 router.get('/', async (req, res) => {
     let searchOptions = {}
@@ -33,8 +34,8 @@ router.post('/', async(req,res) => {
     })
     try{
         const newAuthor = await author.save()  // when using async also use await for async call to be completed 
-        //res.redirect(`authors/${newAuthor.id}`)    // yet to implement
-        res.redirect(`authors`)
+        res.redirect(`authors/${newAuthor.id}`)    // yet to implement
+        //res.redirect(`authors`)
     }
     catch{
         res.render('authors/new',{
@@ -42,19 +43,75 @@ router.post('/', async(req,res) => {
             errorMessage: 'Err author name not saved'
         })
     }
-    // author.save((err,newAuthor) => {
-    //     if(err){
-    //         res.render('authors/new',{
-    //             author: author,
-    //             errorMessage: 'Err author name not saved'
-    //         })
-    //     }
-    //     else{
-    //         //res.redirect(`authors/${newAuthor.id}`)    // yet to implement
-    //         res.redirect(`authors`)
-    //     }
-    // })
     //res.send(req.body.name)
 })
+
+router.get('/:id', async (req,res) => {
+    //res.send("Show Author "+ req.params.id)
+    try{
+        const author = await Author.findById(req.params.id)
+        const books = await Book.find({author: author.id}).limit(6).exec()
+        //console.log(books)
+        res.render('authors/show',{
+            author: author,
+            booksByAuthor: books
+        })
+    }
+    catch{
+        res.redirect('/')
+    }
+})
+
+router.get('/:id/edit', async (req,res) => {
+    //res.send("Edit Author "+ req.params.id)
+    try{
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit',{author: author})
+    }
+    catch{
+        res.redirect('authors')
+    }
+})
+
+router.put('/:id',  async (req,res) => {
+    //res.send("Update Author "+ req.params.id) 
+    let author
+    try{
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()  // when using async also use await for async call to be completed 
+        res.redirect(`/authors/${author.id}`) 
+        //res.redirect(`authors`)
+    }
+    catch{
+        if(author == null){
+            res.redirect('/')
+        }
+        res.render('authors/edit',{
+            author: author,
+            errorMessage: 'Err in author update name'
+        })
+    }
+})
+
+router.delete('/:id', async (req,res) => {
+    //res.send("Delete Author "+ req.params.id)
+    let author
+    try{
+        author = await Author.findById(req.params.id)
+        await author.remove()  // when using async also use await for async call to be completed 
+        res.redirect('/authors') 
+        //res.redirect(`authors`)
+    }
+    catch{
+        if(author == null){
+            res.redirect('/')
+        }
+        else{
+            res.redirect(`/authors/${author.id}`)
+        }
+    }
+})
+
 // this will export for making use from else where in proj
 module.exports = router 
